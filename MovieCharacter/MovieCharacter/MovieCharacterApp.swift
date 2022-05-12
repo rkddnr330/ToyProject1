a0f2bde7891bebdb341ff5b1baff96612361ea05
rkddnr330 <rkddnr330@gmail.com>
2022-05-12 20:24:22 +0900

    new project

---
+++ MovieCharacter/MovieCharacter/MovieCharacterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCharacterApp.swift
+//  MovieCharacter
+//
+//  Created by Park Kangwook on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct MovieCharacterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
